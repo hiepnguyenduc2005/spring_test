a00d653bbef33c09a83072dd01e822ce09c81c85
hiepnguyenduc2005 <hiepnguyenduc2005@gmail.com>
2025-09-13 23:05:17 -0400

    update db and testing

--- src/main/java/com/example/runner/RunnerApplication.java
+++ src/main/java/com/example/runner/RunnerApplication.java
@@ -1,23 +1,14 @@
 package com.example.runner;
 
-import com.example.runner.run.Location;
-import com.example.runner.run.Run;
-import com.example.runner.user.User;
 import com.example.runner.user.UserHttpClient;
-import com.example.runner.user.UserRestClient;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
-
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.List;
 
 @SpringBootApplication
 public class RunnerApplication {
@@ -36,17 +27,25 @@ public class RunnerApplication {
         return factory.createClient(UserHttpClient.class);
     }
 
-    @Bean
-    CommandLineRunner runner(UserRestClient client){
-        return args -> {
-//            Run run = new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
-//            log.info("Run: " + run);
-            List<User> users = client.findAll();
-            System.out.println(users);
+//    @Bean
+//    CommandLineRunner runner(UserRestClient client){
+//        return args -> {
+////            Run run = new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+////            log.info("Run: " + run);
+//            List<User> users = client.findAll();
+//            System.out.println(users);
+//
+//            User user = client.findById(1);
+//            System.out.println(user);
+//        };
+//    }
 
-            User user = client.findById(1);
-            System.out.println(user);
-        };
-    }
+//    @Bean
+//    CommandLineRunner runner(RunRepository runRepository) {
+//        return args -> {
+//            Run run = new Run(1, "Morning Run", LocalDateTime.now(), LocalDateTime.now().plus(1, ChronoUnit.HOURS), 5, Location.OUTDOOR);
+//            runRepository.create(run);
+//        };
+//    }
 
 }
--- src/main/java/com/example/runner/run/JdbcClientRunController.java
+++ src/main/java/com/example/runner/run/JdbcClientRunController.java
@@ -3,19 +3,19 @@ package com.example.runner.run;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-@RequestMapping("/api/runs")
+@RequestMapping("/api/jdbc_runs")
 @RestController
-public class RunController {
-    private final RunRepository runRepository;
+public class JdbcClientRunController {
+    private final JdbcClientRunRepository runRepository;
 
-    public RunController(RunRepository runRepository) {
+    public JdbcClientRunController(JdbcClientRunRepository runRepository) {
         this.runRepository = runRepository;
     }
+
 
     @GetMapping("")
     List<Run> findAll() {
